acc9180a26f22e5e60d0c2eef41b6b6858160cc0
itsDenMat <dennismateo93@gmail.com>
2022-08-23 16:01:49 -0700

    Add Engineer.test.js

---
+++ _test_/Engineer.test.js
@@ -0,0 +1,27 @@
+// Import Engineer.js
+const { default: test } = require("node:test");
+const { hasUncaughtExceptionCaptureCallback } = require("process");
+const Engineer = require("../lib/Engineer");
+
+// TEST FOR ENGINEER
+// Create GitHub check
+test("Can a GitHub link.", () =>{
+    const testGitHub = "JimmyGit";
+    const employeeTest = new Engineer("Jimmy", 3, "jimmy@gmail.com", testGitHub);
+    expect(employeeTest.github).toBe(testGitHub);
+});
+
+// TEST FOR RETRIEVAL USING METHODS in Engineer.js
+// getGithub() test
+test("Can it retrieve GitHub through getGithub method.", () => {
+    const testGitHub = "JimmyGit";
+    const employeeTest = new Engineer("Jimmy", 3, "jimmy@gmail.com", "JimmyGit");
+    expect(employeeTest.getRole()).toBe(returnValue);
+});
+
+// getRole() test
+test("Checking role.", () => {
+    const returnValue = "Engineer";
+    const employeeTest = new Engineer("James", 3, "jimmy@gmail.com", "JimmyGit");
+    expect(employeeTest.getRole()).toBe(returnValue);
+})
